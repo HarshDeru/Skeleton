5f60a40858abd6f74bf68be85972ed99d3e5f779
Shilpesh Jentilal <P2598688@my365.dmu.ac.uk>
2022-02-10 15:15:11 +0000

    Added a class for staff, which checks if the class exists within the system.

---
+++ Testing1/tstStaff.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void tstInstanceOK()
+        {
+            clsStaff Staff = new clsStaff();
+            Assert.IsNotNull(Staff);
+        }
+    }
+}
