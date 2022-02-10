0aead1f8ad155d96d5403c55e9ad0b888caa5697
Harsh Deru <P2588964@my365.dmu.ac.uk>
2022-02-10 16:00:05 +0000

    Added testing for order class

---
+++ Testing2/tstOrder.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+
+            clsOrder AnOrder = new clsOrder();
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
