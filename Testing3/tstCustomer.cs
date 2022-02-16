aa67b292e6d2cfd00475ed210229079b414627bc
Ujwal Dharmesh <P2603348@my365.dmu.ac.uk>
2022-02-16 11:29:17 +0000

    Added Class and Testing for Customer system.

---
+++ Testing3/tstCustomer.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { 
+                clsCustomer ACustomer = new clsCustomer();
+                Assert.IsNotNull(ACustomer);
+            }
+        }
+    }
+
