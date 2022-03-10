afcc2122bfe0ec87cad6cddc87ed92a00a606d3c
Tanay Suresh <P2590716@my365.dmu.ac.uk>
2022-03-10 13:38:43 +0000

    Added clsStock and tstStock in stock.

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Active { get; set; }
+        public int ProductID { get; set; }
+        public double Product_Price { get; set; }
+        public bool Product_Availablilty { get; set; }
+        public DateTime Product_Shipped { get; set; }
+        public int Product_Quantity { get; set; }
+        public string Product_Name { get; set; }
     }
 }
