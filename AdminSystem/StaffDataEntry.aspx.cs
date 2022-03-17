31bd839e94a014e301f0fb97f045c3acc834c010
Shilpesh Jentilal <P2598688@my365.dmu.ac.uk>
2022-03-17 14:47:25 +0000

    Created working directory and also added error label when wrong id is entered.

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -12,7 +12,7 @@ public partial class _1_DataEntry : System.Web.UI.Page
     {
     }
 
- 
+
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
@@ -25,7 +25,7 @@ public partial class _1_DataEntry : System.Web.UI.Page
         AStaff.Staff_Role = txtStaffRole.Text;
         AStaff.Staff_HireDate = Convert.ToDateTime(txtStaffHireDate.Text);
         AStaff.Staff_Salary = Convert.ToInt32(txtStaffSalary.Text);
-        AStaff.Staff_Gender = Convert.ToBoolean(dblGender.SelectedIndex);
+        AStaff.Staff_Gender = Convert.ToBoolean(dblGender.Text);
         //store the staff in the session object
         Session["AStaff"] = AStaff;
         //redirects to staff viewer page.
@@ -33,7 +33,7 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     }
 
-  
+
 
 
 
@@ -41,4 +41,37 @@ public partial class _1_DataEntry : System.Web.UI.Page
     {
 
     }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        clsStaff Staff = new clsStaff();
+        Int32 StaffID;
+        Boolean Found = false;
+        StaffID = Convert.ToInt32(txtStaffID.Text);
+        Found = Staff.Find(StaffID);
+
+        if (Found == true)
+        {
+            lblError.Text = "";
+            txtStaffName.Text = Staff.Staff_FullName;
+            txtStaffRole.Text = Staff.Staff_Role;
+            txtStaffSalary.Text = Staff.Staff_Salary.ToString();
+            txtStaffHireDate.Text = Staff.Staff_HireDate.ToString();
+            dblGender.SelectedValue = Staff.Staff_Gender.ToString();
+
+        }
+        else
+        {
+            txtStaffName.Text = "";
+            txtStaffRole.Text = "";
+            txtStaffSalary.Text = "";
+            txtStaffHireDate.Text = "";
+
+            lblError.Text = "Error: This ID does not exist!";
+            lblError.Visible = true;
+        }
+    }
 }
+
+
+
