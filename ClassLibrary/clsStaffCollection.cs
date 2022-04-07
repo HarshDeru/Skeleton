a707e5f4289d07f8c586bd6fa47b0fbd5833043d
Shilpesh Jentilal <P2598688@my365.dmu.ac.uk>
2022-04-07 12:28:35 +0100

    Successfully methods and tests to test the method is working fine for staff list and created a list view for it.Tested and no erorrs.

---
+++ ClassLibrary/clsStaffCollection.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsStaffCollection
+    {
+        List<clsStaff> mStaffList = new List<clsStaff>();
+        public List<clsStaff> StaffList
+        {
+            get
+            {
+                return mStaffList;
+            }
+            set
+            {
+                mStaffList = value;
+            }
+        }
+
+
+
+        public int Count
+        {
+            get
+            {
+                return mStaffList.Count;
+            }
+            set
+            {
+
+            }
+        }
+
+        public clsStaffCollection()
+        {
+            Int32 Index = 0;
+
+            Int32 RecordCount = 0;
+
+            clsDataConnection DB = new clsDataConnection();
+
+            DB.Execute("sproc_tblStaff_SelectAll");
+
+            RecordCount = DB.Count;
+
+            while (Index < RecordCount)
+            {
+                clsStaff Staff = new clsStaff();
+                Staff.StaffID = Convert.ToInt32(DB.DataTable.Rows[Index]["StaffID"]);
+                Staff.Staff_FullName = Convert.ToString(DB.DataTable.Rows[Index]["Staff_FullName"]);
+                Staff.Staff_Gender = Convert.ToBoolean(DB.DataTable.Rows[Index]["Staff_Gender"]);
+                Staff.Staff_Role = Convert.ToString(DB.DataTable.Rows[Index]["Staff_Role"]);
+                Staff.Staff_HireDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["Staff_HireDate"]);
+                Staff.Staff_Salary = Convert.ToDouble(DB.DataTable.Rows[Index]["Staff_Salary"]);
+
+                mStaffList.Add(Staff);
+                Index++;
+            }
+            
+           
+
+
+        }
+    }
+}
